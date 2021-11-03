9bdaf29ed48082a21ab1b9fbb28bd3bc2c36c040
fillemon10 <filip.sjolander@gmail.com>
2021-11-03 16:19:33 +0100

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Good morng, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Good, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 # NEW COMMENT -
 # Press the green button in the gutter to run the script.
